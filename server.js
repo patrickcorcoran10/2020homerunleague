e24b698248583ae892e813732fd5ca0023cb88c6
Patty C <patrickcorcoran10@gmail.com>
2020-08-03 11:21:18 -0500

    commit with initial code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// const db = require("./models");
+
+// require("./routes/homerun-db-routes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+// db.sequelize.sync().then(function () {
+app.listen(PORT, function () {
+  console.log("We are swinging for the fences at ", PORT);
+});
+// });
